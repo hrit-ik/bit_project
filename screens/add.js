d4254008bd1c37cdc6c3b921d14aa8f1ba0e6dcf
Hritik Chaudhary <hritikplay@gmail.com>
2021-12-21 00:27:41 +0530

    added image picker and connected firestore

--- screens/add.js
+++ screens/add.js
@@ -1,30 +1,80 @@
-import React from 'react'
-import { View, Text, TextInput, StyleSheet, Button} from 'react-native'
+import React, {useState, useRef} from 'react'
+import { View, Text, TextInput, StyleSheet, Button, Image} from 'react-native'
+import * as ImagePicker from 'expo-image-picker';
+import {db, storage} from '../Backend/firestore'
+import { collection, addDoc, Timestamp } from "firebase/firestore"; 
+import { auth } from "../Backend/firebase";
+import { useUpload } from '../Hooks/useUpload';
 
 const Add = () => {
+    const [image, setImage] = useState(null);
+    const [eventName, setEventName] = useState('');
+    const [eventDate, setEventDate] = useState('');
+    const [eventTime, setEventTime] = useState('');
+    const [eventDescription, setEventDescription] = useState('');
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+        useUpload('images', result.uri);
+    }
+  };
+
+  const docAdd = async()=>{
+    try {
+        const docRef = await addDoc(collection(db, "Events"), {
+            eventName: eventName,
+            eventDate: eventDate,
+            eventTime: eventTime,
+            eventDescription: eventDescription,
+            createdAt: Timestamp.now(),
+            // postedBy: auth.currentUser?.email,
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }      
+  }
+
     return (
             <View style={styles.container}>
+                <Button title="Pick an image" onPress={pickImage} />
+                {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
                 <TextInput
                     placeholder="Event Name"
                     style={styles.input}
+                    onChangeText={(text) => setEventName(text)}
                 />
                 <TextInput
                     placeholder="Event Date"
                     style={styles.input}
+                    onChangeText={(text) => setEventDate(text)}
                 />
                 <TextInput
                     placeholder="Event Time"
                     style={styles.input}
+                    onChangeText={(text) => setEventTime(text)}
                 />
                 <TextInput
-                    // multiline={true}
+                    multiline={true}
                     placeholder="Event Description"
-                    style={styles.input}
+                    style={[styles.input]}
+                    onChangeText={(text) => setEventDescription(text)}
                 />
                 <View style={styles.buttonContainer}>
                 <Button
                     title="Add Event"
-                    onPress={() => {}}
+                    onPress={()=>{docAdd(eventName, eventDate, eventTime, eventDescription)}}
                     color={'white'}
                 />
                 </View>
